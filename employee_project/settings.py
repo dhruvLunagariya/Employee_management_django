2f64570f748dd3114539adfa7fd84d8d4579f828
Dhruv Lunagariya <96902854+dhruvLunagariya@users.noreply.github.com>
2023-08-20 18:05:08 +0530

    gh

--- employee_project/settings.py
+++ employee_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ejc@3vkshtg6*9$)f^n@qw=ej2_2^*m++-in24cjp(+^huug$+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh','*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,9 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-import os
-# STATICFILES_DIRS=os.path.join(BASE_DIR,'static')
-# STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
+
 
 
 # Default primary key field type
